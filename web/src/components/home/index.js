60210688283b47597c48c03592b69bde600310d1
Eytyy <e.tayyem@gmail.com>
2021-07-13 22:18:25 +0300

    Move layout related code to layout component and move work component to home component

--- web/src/components/home/index.js
+++ web/src/components/home/index.js
@@ -1,7 +1,22 @@
 import React from "react"
+import { useSiteContext } from "../../siteContext"
+
+import ProjectPreview from "../project/Preview"
+
+import { ProjectsWrapper } from "./styles"
 
 const Home = () => {
-  return <></>
+  const { data } = useSiteContext()
+
+  return (
+    data?.projects?.all && (
+      <ProjectsWrapper>
+        {data.projects.all.map(project => (
+          <ProjectPreview {...project} key={project.id} />
+        ))}
+      </ProjectsWrapper>
+    )
+  )
 }
 
 export default Home
--- web/src/components/layout/index.js
+++ web/src/components/layout/index.js
@@ -2,19 +2,19 @@ import React, { useLayoutEffect } from "react"
 import PropTypes from "prop-types"
 
 import Global from "../../styles/Global.js"
-import Work from "../work/index.js"
+import { LayoutWrapper } from "./styles.js"
+import Header from "../header/index.js"
+import Navigation from "../navigation/index.js"
 
 const Layout = ({ children, location }) => {
   // alternative way to using full view port height instead of 100vh
   // to avoid common layout issues on mobile.
-
   useLayoutEffect(() => {
     const appHeight = () => {
       const doc = document.documentElement
       doc.style.setProperty("--app-height", `${window.innerHeight}px`)
     }
     appHeight()
-
     window.addEventListener("resize", appHeight)
     return function cleanup() {
       window.removeEventListener("resize", appHeight)
@@ -23,9 +23,12 @@ const Layout = ({ children, location }) => {
 
   return (
     <>
-      <Global />
-      <main>{children}</main>
-      <Work />
+      <LayoutWrapper>
+        <Header />
+        <Navigation />
+        <Global />
+        <main>{children}</main>
+      </LayoutWrapper>
     </>
   )
 }
